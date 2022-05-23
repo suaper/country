62e1d48328a4726a01cc51112259fab84d2723ca
Jhonny Gil <jhonny.gil@markablanka.com>
2022-05-23 17:32:06 -0500

    Âdf

--- src/pages/eventos/TipoEventos.vue
+++ src/pages/eventos/TipoEventos.vue
@@ -1,24 +1,71 @@
 <template>
   <q-page class="flex flex-center view_hijos_socios view_fitness">
-    <Menueventos currentItem="/eventos/tipo-eventos"/>
-    <div class="q-py-none all_width">
-      <q-carousel
-        animated
-        v-model="slide"
-        arrows
-        class="banner_top"
-        navigation
-        infinite
-      >
-        <q-carousel-slide v-for="(banner, key) in info.field_banner_seccion" :key="key" :name="banner.target_uuid" :img-src="banner.url" />
-      </q-carousel>
+  <Menueventos currentItem="/eventos/tipo-eventos"/>
+  <div class="q-py-none all_width">
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      class="banner_top"
+      navigation
+      infinite
+    >
+      <q-carousel-slide v-for="(banner, key) in info.field_banner_seccion" :key="key" :name="banner.target_uuid" :img-src="banner.url" />
+    </q-carousel>
+  </div>
+  <div class="q-pb-md all_width gris_home">
+      <div class="cincuenta q-pd-md centrar text-center">
+          <div class="center text-center q-my-lg titulos">{{ info.title[0].value }}</div>
+          <p class="intro text-center" v-html="info.body[0].value"></p>
+      </div>
+  </div>
+
+  <div class="q-py-xl all_width bg_amarillo wrp_club hazte_socio">
+      <div class="centrar w_1200">
+          <ul class="list_tipos_eventos">
+              <li>
+                  <div class="wrpleft">
+                    <img src="../../assets/MiClub/i-pdf.svg">
+                  </div>
+                  <div class="wrpright">
+                      <strong>Almuerzos y Comidas</strong>
+                      <a href="#"> Ver mas</a>
+                  </div>
+              </li>
+
+              <li>
+                  <div class="wrpleft">
+                    <img src="../../assets/MiClub/i-pdf.svg">
+                  </div>
+                  <div class="wrpright">
+                      <strong>Almuerzos y Comidas</strong>
+                      <a href="#"> Ver mas</a>
+                  </div>
+              </li>
+
+              <li>
+                  <div class="wrpleft">
+                    <img src="../../assets/MiClub/i-pdf.svg">
+                  </div>
+                  <div class="wrpright">
+                      <strong>Almuerzos y Comidas</strong>
+                      <a href="#"> Ver mas</a>
+                  </div>
+              </li>
+
+              <li>
+                  <div class="wrpleft">
+                    <img src="../../assets/MiClub/i-pdf.svg">
+                  </div>
+                  <div class="wrpright">
+                      <strong>Almuerzos y Comidas</strong>
+                      <a href="#"> Ver mas</a>
+                  </div>
+              </li>
+          </ul>
+      </div>
     </div>
-    <div class="q-pb-md all_width gris_home">
-        <div class="cincuenta q-pd-md centrar text-center">
-            <div class="center text-center q-my-lg titulos">{{ info.title[0].value }}</div>
-            <p class="intro text-center" v-html="info.body[0].value"></p>
-        </div>
-    </div>
+
   <div :class="(key % 2 === 0) ? 'q-py-xl all_width bg_amarillo wrp_club hazte_socio' : 'q-py-xl all_width gris_home wrp_club hazte_socio'" v-for="(item, key) in eventTypes" :key="key">
         <div class="centrar w_1200">
           <table class="contenido_fitness q-my-md">
