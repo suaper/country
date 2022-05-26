5de8331e7a247da54546d57d0d6aac8ca47f3a62
Alejandro Suárez <alejandro.suarez@dualbootpartners.com>
2022-05-26 14:08:53 -0500

    Buscador libros

--- src/pages/cultura/BuscadorBiblioteca.vue
+++ src/pages/cultura/BuscadorBiblioteca.vue
@@ -4,92 +4,44 @@
     <div class="q-pb-md all_width gris_home view_danzas q-my-xl">
         <div class="setenta  centrar text-center relative">
             <div class="center text-center q-my-lg titulos">Buscador de Libros</div>
-            <div class="back"> <q-btn to="/cultura/danza" round color="white" icon="west" />Volver</div>
+            <div class="back"> <q-btn to="/cultura/biblioteca" round color="white" icon="west" />Volver</div>
         </div>
     </div>
     <div class="q-py-md w_1200 centrar flex_escuelas flex_obras justify-center">
         <div class="row_search">
             <div class="wrp_search flex">
                 <h5>Buscador de Libros</h5>
-                <q-select
-                filled
-                v-model="model"
-                use-input
-                input-debounce="0"
-                label="Simple filter"
-                :options="options"
-                @filter="filterFn"
-                style="width: 250px"
-                behavior="menu"
-            >
-                <template v-slot:no-option>
-                <q-item>
-                    <q-item-section class="text-grey">
-                    No results
-                    </q-item-section>
-                </q-item>
-                </template>
-            </q-select>
+                <q-input
+                    outlined
+                    v-model="filter"
+                    lazy-rules
+                    :rules="[ val => val && val.length > 3 || 'Debe escribir más de 3 letras']"
+                    @input="setFilter()"
+                />
             </div>
             <div class="radio">
                 <p>Filtros</p>
                 <div class="q-pa-md vertical">
-                    <q-radio v-model="shape" val="ingles" color="orange" label="Inglés" />
-                    <q-radio v-model="shape" val="espaniol" color="orange"  label="Español" />
+                    <q-radio v-model="shape" val="ingles" color="orange" label="Inglés" @input="setFilterLanguage"/>
+                    <q-radio v-model="shape" val="español" color="orange"  label="Español" @input="setFilterLanguage"/>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="q-pb-xl all_width bg_amarillo wrp_club hazte_socio">
+    <div class="q-pb-xl all_width bg_amarillo wrp_club hazte_socio" v-if="filtersSeted">
         <div class="centrar w_1200">
         <div class="text-left q-mb-none q-mt-xl titulos wrp_gallery_biblioteca">Resultados de Búsqueda</div>
 
             <div class="row fit justify-between items-center q-gutter-xs q-col-gutter wrap wwsearcing">
-                <div class="treintaydos_general search_box">
+                <div class="treintaydos_general search_box" v-for="(book, keyBook) in booksFilter" :key="keyBook">
                     <table class="galeri_event">
                         <tr>
                             <td class="center">
-                                <img class="img_biblioteca" src="https://pwccdev.mkbk.digital//administrador/sites/default/files/2022-03/libro01.png" />
-                                <h5 class="name_event">Libro 5</h5>
-                                <p class="desc_event text-center">Lorem ipsum dolor sit amet consectetur </p>
-                                <a href="#">Reservar<q-icon name="arrow_right_alt" class="cursor-pointer"></q-icon></a>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-              <div class="treintaydos_general search_box">
-                    <table class="galeri_event">
-                        <tr>
-                            <td class="center">
-                                <img class="img_biblioteca" src="https://pwccdev.mkbk.digital//administrador/sites/default/files/2022-03/libro01.png" />
-                                <h5 class="name_event">Libro 5</h5>
-                                <p class="desc_event text-center">Lorem ipsum dolor sit amet consectetur </p>
-                                <a href="#">Reservar<q-icon name="arrow_right_alt" class="cursor-pointer"></q-icon></a>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-              <div class="treintaydos_general search_box">
-                    <table class="galeri_event">
-                        <tr>
-                            <td class="center">
-                                <img class="img_biblioteca" src="https://pwccdev.mkbk.digital//administrador/sites/default/files/2022-03/libro01.png" />
-                                <h5 class="name_event">Libro 5</h5>
-                                <p class="desc_event text-center">Lorem ipsum dolor sit amet consectetur </p>
-                                <a href="#">Reservar<q-icon name="arrow_right_alt" class="cursor-pointer"></q-icon></a>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-              <div class="treintaydos_general search_box">
-                    <table class="galeri_event">
-                        <tr>
-                            <td class="center">
-                                <img class="img_biblioteca" src="https://pwccdev.mkbk.digital//administrador/sites/default/files/2022-03/libro01.png" />
-                                <h5 class="name_event">Libro 5</h5>
-                                <p class="desc_event text-center">Lorem ipsum dolor sit amet consectetur </p>
-                                <a href="#">Reservar<q-icon name="arrow_right_alt" class="cursor-pointer"></q-icon></a>
+                                <img class="img_biblioteca" :src="urlSite + book.field_imagen_libro" />
+                                <h5 class="name_event">{{ book.title }}</h5>
+                                <p class="desc_event text-center" v-html="book.body"></p>
+                                <q-btn class="btn_bg_beige centrar  btn_centrar" :label="book.field_tipo_de_libro" @click="openDetail(book)"/>
                             </td>
                         </tr>
                     </table>
@@ -183,25 +135,6 @@
             </q-card-section>
         </q-card>
     </q-dialog>
-    <div class="q-pb-xl all_width bg_gris wrp_club hazte_socio">
-        <div class="centrar w_1200">
-        <div class="text-left q-mb-none q-mt-xl titulos">Literatura Infantil</div>
-            <div class="row fit justify-between items-center q-gutter-xs q-col-gutter no-wrap">
-                <div class="treintaydos_general" v-for="(book, keyBook) in item" :key="keyBook">
-                    <table class="galeri_event">
-                        <tr>
-                            <td>
-                                <img class="img_biblioteca" :src="urlSite + book.field_imagen_libro" />
-                                <h5 class="name_event">{{ book.title }}</h5>
-                                <p class="desc_event text-center" v-html="book.body"></p>
-                                <q-btn class="btn_bg_beige centrar  btn_centrar" :label="book.field_tipo_de_libro" @click="openDetail(book)"/>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
 
   </q-page>
 </template>
@@ -210,34 +143,7 @@
 import Menucultura from 'pages/submenus/Menucultura'
 import configServices from '../../services/config'
 
-const stringOptions = [
-  'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-]
-
 export default {
-  setup () {
-    const options = stringOptions
-
-    return {
-      model: null,
-      stringOptions,
-      options,
-
-      filterFn (val, update) {
-        if (val === '') {
-          update(() => {
-            options.value = stringOptions
-          })
-          return
-        }
-
-        update(() => {
-          const needle = val.toLowerCase()
-          options.value = stringOptions.filter(v => v.toLowerCase().indexOf(needle) > -1)
-        })
-      }
-    }
-  },
   name: 'Biblioteca',
   components: {
     Menucultura
@@ -249,7 +155,8 @@ export default {
       slideMoreReads: 0,
       currentBook: {},
       slideChild: 0,
-      shape: 'ingles',
+      filter: '',
+      shape: '',
       info: {
         body: [
           { value: '' }
@@ -265,16 +172,42 @@ export default {
       booksChild: [],
       personal: [],
       detalle: false,
-      formulario: false
+      formulario: false,
+      telefono: '',
+      correo: '',
+      name: '',
+      rut: '',
+      apellido: '',
+      booksFilter: [],
+      filtersSeted: false
     }
   },
   created () {
-    this.getInfo()
-    this.getMultimediaHome()
-    this.getBooksMoreReads()
-    this.getBooksChild()
+    this.getBooks()
   },
   methods: {
+    setFilter () {
+      var _this = this
+      if (this.filter.length > 3) {
+        _this.booksFilter = []
+        this.books.map((item, key) => {
+          if (item.title.toLowerCase().includes(this.filter) || item.body.toLowerCase().includes(this.filter) || item.field_autor.toLowerCase().includes(this.filter) || item.field_categoria_libro.toLowerCase().includes(this.filter)) {
+            _this.booksFilter.push(item)
+          }
+        })
+        _this.filtersSeted = true
+      }
+    },
+    setFilterLanguage () {
+      var _this = this
+      _this.booksFilter = []
+      this.books.map((item, key) => {
+        if (item.field_idioma_libro.toLowerCase() === _this.shape) {
+          _this.booksFilter.push(item)
+        }
+      })
+      _this.filtersSeted = true
+    },
     onSubmit () {
       var _this = this
       var data = {
@@ -305,21 +238,6 @@ export default {
     onReset () {
       // reset
     },
-    getInfo () {
-      var _this = this
-      configServices.loadData(this, '/node/201?_format=json', {
-        callBack: (data) => {
-          _this.info = data
-          _this.slide = data.field_slider_home[0].target_uuid
-        }
-      })
-
-      configServices.loadData(this, '/personal-staff/biblioteca', {
-        callBack: (data) => {
-          _this.personal = data[0]
-        }
-      })
-    },
     openDetail (book) {
       this.currentBook = book
       this.detalle = true
@@ -331,36 +249,12 @@ export default {
     cerrarFormulario () {
       this.formulario = false
     },
-    getMultimediaHome () {
+    getBooks () {
       var _this = this
-      configServices.loadData(this, 'multimedia-home/json', {
+      configServices.loadData(this, '/libros/json', {
         callBack: (data) => {
-          for (const item in data) {
-            _this.multimediaHome.push(data[item])
-          }
+          _this.books = data
           _this.$q.loading.hide()
-        }
-      })
-    },
-    getBooksMoreReads () {
-      var _this = this
-      configServices.loadData(this, '/libros/50/json', {
-        callBack: (data) => {
-          const n = 3
-          _this.booksMoreReads = new Array(Math.ceil(data.length / n))
-            .fill()
-            .map(_ => data.splice(0, n))
-        }
-      })
-    },
-    getBooksChild () {
-      var _this = this
-      configServices.loadData(this, '/libros/49/json', {
-        callBack: (data) => {
-          const n = 3
-          _this.booksChild = new Array(Math.ceil(data.length / n))
-            .fill()
-            .map(_ => data.splice(0, n))
         }
       })
     },
