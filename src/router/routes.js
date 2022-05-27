1fe5377f5232bbdf22882d8e9befe20680776895
Jhonny Gil <jhonny.gil@markablanka.com>
2022-05-27 14:11:54 -0500

    vista buscador

--- src/router/routes.js
+++ src/router/routes.js
@@ -57,6 +57,13 @@ const routes = [
     ]
   },
   {
+    path: '/vista-buscador',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/VistaBuscador.vue') }
+    ]
+  },
+  {
     path: '/mi-club',
     component: () => import('layouts/MainLayout.vue'),
     children: [
