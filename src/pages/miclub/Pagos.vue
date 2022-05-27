db6bd914f5e98bbe40da56ce1d8bab8ca4a366a3
Jhonny Gil <jhonny.gil@markablanka.com>
2022-05-27 10:28:08 -0500

    d

---
+++ src/pages/miclub/Pagos.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page class="flex flex-center view_quienes_somos">
+    <Menumiclub currentItem="/mi-club/cuotas-reglamento" />
+    <div class="q-pb-md all_width gris_home etiquetas">
+        <br>
+        <div class="center text-center q-my-lg titulos q-mt-2">Pagos</div>
+        <div class="centrar w_1200 pagos">
+            <div class="left text-left q-my-lg titulos">Pago de Cuota de Incorporación</div>
+            <div class="q-pa-md row items-start q-gutter-md no-flex">
+                <p>Corporación Principe de Gales</p>
+                <p>Rut 12123223123</p>
+                <p>Corporación Principe de Gales</p>
+                <p>Rut 12123223123</p>
+                <p>Rut 12123223123</p>
+            </div>
+            <div class="text-right">
+                <q-btn outline class="azul q-my-md centrar bg_white_i" label="Ver Bases" icon-right="arrow_right_alt"/>
+                <img src="https://pwccdev.mkbk.digital/administrador/sites/default/files/2022-05/bancobice.png">
+            </div>
+            <div class="left text-left q-my-lg titulos">Pago de Cuota de Incorporación</div>
+            <div class="q-pa-md row items-start q-gutter-md no-flex">
+                <p>Corporación Principe de Gales</p>
+                <p>Rut 12123223123</p>
+                <p>Corporación Principe de Gales</p>
+                <p>Rut 12123223123</p>
+                <p>Rut 12123223123</p>
+            </div>
+        </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import Menumiclub from 'pages/submenus/Menumiclub'
+
+export default {
+  name: 'Pagos',
+  components: {
+    Menumiclub
+  }
+}
+</script>
--- src/pages/submenus/Menumiclub.vue
+++ src/pages/submenus/Menumiclub.vue
@@ -1,5 +1,6 @@
 <template>
     <q-btn-group outline rounded class="sub_menu open">
+      <q-btn outline :disable="getDisable('/mi-club/pagos')" @click="goItem('/mi-club/pagos')"  color="submenu" label="Pagos" />
       <q-btn outline :disable="getDisable('/mi-club')" color="submenu" label="Clubes de intercambio" @click="goItem('/mi-club')"/>
       <q-btn outline :disable="getDisable('/mi-club/etiquetas')" @click="goItem('/mi-club/etiquetas')" color="submenu" label="Normas de Convivencia" />
       <q-btn outline :disable="getDisable('/mi-club/cuotas-reglamento')" @click="goItem('/mi-club/cuotas-reglamento')" color="submenu" label="Cuotas Sociales y Reglamento" />
