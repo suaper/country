575af156daa3836dfd5c4cbd9a1995ef9965b4e1
Jhonny Gil <jhonny.gil@markablanka.com>
2022-08-03 12:12:29 -0500

    novedades

---
+++ src/pages/submenus/Menuspecialdays.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="new_wrap">
+    <q-btn-group outline rounded class="sub_menu open">
+      <q-btn outline :disable="getDisable('/special-days')"  @click="goItem('/special-days')" color="submenu" label="Special Days" />
+      <q-btn outline :disable="getDisable('/special-days/multimedia')"  @click="goItem('/special-days/multimedia')" color="submenu" label="Multimedia" />
+    </q-btn-group>
+    <Buscador />
+  </div>
+</template>
+
+<script>
+import Buscador from './Buscador.vue'
+
+export default {
+  name: 'Menuspecialdays',
+  components: { Buscador },
+  props: {
+    currentItem: String
+  },
+  data () {
+    return {
+      current: this.currentItem
+    }
+  },
+  methods: {
+    goItem (route) {
+      this.$router.push(route)
+    },
+    getDisable (route) {
+      if (this.current === route) return true
+      return false
+    }
+  }
+}
+</script>
