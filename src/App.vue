984b6982017b748087d2669fbd981ae6e5145764
Alejandro Suárez <alejandro.suarez@dualbootpartners.com>
2022-08-18 15:28:11 -0500

    Analytics

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,11 @@ const options = {
 
 Vue.use(VueSweetalert2, options)
 import 'sweetalert2/dist/sweetalert2.min.css'
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-237083350-1'
+})
 
 export default {
   name: 'App'
